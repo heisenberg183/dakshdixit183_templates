01bae44aac1d6b44f534f85d07e54d9b32629242
Daksh Dixit <dakshdixit183@gmail.com>
2024-03-13 17:01:43 +0530

    LCA for wts

--- Trees/WeightedTreeLCA.cpp
+++ Trees/WeightedTreeLCA.cpp
@@ -24,6 +24,7 @@ const int mod = 1000000007;
 #define PI 3.141592653589793238462
 #define set_bits(x) __builtin_popcountll(x)
 #define all(x) (x).begin(), (x).end()
+#define rsz(arr,n) arr.clear();arr.resize(n)
 #define print(x) cout<<x<<"\n"
 #define fr(i,a,b) for(int i = a; i < b; i++)
 #define fo(i,a,b) for(int i=a;i<=b;i++)
@@ -95,24 +96,22 @@ int mod_mul(int a, int b) {a = a % mod; b = b % mod; return (((a * b) % mod) + m
 int mod_sub(int a, int b) {a = a % mod; b = b % mod; return (((a - b) % mod) + mod) % mod;}
 int mod_div(int a, int b) {a = a % mod; b = b % mod; return (mod_mul(a, mminvprime(b)) + mod) % mod;}  //only for prime m
 int ceil_div(int a, int b) {return a % b == 0 ? a / b : a / b + 1;}
+int ceil_rem(int a, int b) {return a%b==0 ? b : a%b;}
 int getRandomNumber(int l, int r) {return uniform_int_distribution<int>(l, r)(rng);} 
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-// Code and Explanation by Errichto 
-// Tested on Company Queries 2 CSES
-// 0 based Indexing 
-
+vector<vpi> adj;
 const int LOG = 20;
-vector<vi> adj;
-vector<vi> up;
+vector<vi> up,upDist;
 vi ht;
- 
+
 void dfs(int v,int p=-1){
-    for(auto u : adj[v]){
+    for(auto [u,c] : adj[v]){
         if(u!=p){
             ht[u]=ht[v]+1;
             dfs(u,v);
             up[u][0]=v;
+            upDist[u][0]=c;
         }
     }
 }
@@ -156,58 +155,116 @@ int lca(int a,int b){
 	return up[a][0];
 }
 
-void solve(){
-    int n,q;
-    cin>>n>>q;
-    adj.clear();adj.resize(n);//n+1
-    up.clear();up.resize(n,vector<int>(LOG,-1));//n+1
-    ht.clear();ht.resize(n,0);//n+1
-    for(int i=1;i<n;i++){
-        int j;
-        cin>>j;
-        j--;
-        adj[i].pb(j);
-        adj[j].pb(i);
+int Dist(int a,int b){
+    if(ht[a]<ht[b]){
+        swap(a,b);
     }
-    //Kth Ancestor and LCA precomputation 
-    dfs(0);//n+1
+    int k = ht[a]-ht[b];
+    int ans = 0;
+    // 1) Get same depth.
+    for(int i=30;i>=0;i--){
+        if(k&(1ll<<i)){
+            ans += upDist[a][i];
+            a = up[a][i];
+        }
+    }
+    // 2) if b was ancestor of a then now a==b
+	if(a == b) {
+		return ans;
+	}
+	// 3) move both a and b with powers of two
+	for(int j = LOG - 1; j >= 0; j--) {
+		if(up[a][j] != up[b][j]) {
+            ans += upDist[a][j];
+            ans += upDist[b][j];
+			a = up[a][j];
+			b = up[b][j];
+		}
+	}
+    ans += upDist[a][0];
+	if(up[a][0]!=b){
+        ans += upDist[b][0];
+    }
+    return ans;
+}
+
+
+void solve(){
+    int n;
+    cin>>n;
+    adj.clear();adj.resize(n+1);    
+    up.clear();up.resize(n+1,vector<int>(LOG,-1));
+    upDist.clear();upDist.resize(n+1,vector<int>(LOG,0));
+    ht.clear();ht.resize(n+1,0);
+    for(int i=0;i<n-1;i++){
+        int a,b,h;
+        cin>>a>>b>>h;
+        adj[a].pb({b,h});
+        adj[b].pb({a,h});
+    }
+    debug(adj);
+    dfs(1);
     for(int bit=1;bit<LOG;bit++){
-        for(int v=0;v<n;v++){//n+1
+        for(int v=1;v<=n;v++){
             if(up[v][bit-1]!=-1){
                 up[v][bit]=up[up[v][bit-1]][bit-1];
             }
+            if(up[v][bit]!=-1){
+                upDist[v][bit] = upDist[v][bit-1] + upDist[up[v][bit-1]][bit-1];
+            }
         }
     }
-    while(q--){
-        int a,b;
-        cin>>a>>b;
-        a--,b--;
-        int ans = lca(a,b);
-        ans++;
-        cout<<ans<<endl;
+    while(1){
+        string type;cin>>type;
+        if(type=="DONE"){
+            break;
+        }
+        else if(type=="DIST"){
+            int a,b;
+            cin>>a>>b;
+            if(lca(a,b)!=-1){
+                cout<<Dist(a,b)<<endl;
+            }
+        }
+        else if(type=="KTH"){
+            int a,b,k;
+            cin>>a>>b>>k;
+            int l = lca(a,b);
+            debug(l);
+            debug(ht[a]);
+            debug(ht[b]);
+            debug(ht[l]);
+            if(ht[a]-ht[l]+1>=k){
+                cout<<get(a,k-1)<<endl;
+            }
+            else{
+                k -= (ht[a]-ht[l]+1);
+                int newK = ht[b]-ht[l]-k;
+                cout<<get(b,newK)<<endl;
+            }
+        }
     }
-    debug(up);
 }
 
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
 signed main(){
-    fast;
-    auto start1 = high_resolution_clock::now();
-    // #ifndef ONLINE_JUDGE
-    // freopen("shell.in", "r", stdin);
-    // freopen("shell.out", "w", stdout);
-    // #endif
-    int t=1;
-    // cin>>t;
-    for(int i=1;i<=t;i++){
-        //google(i);
-        solve();
-    }
-    auto stop1 = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop1 - start1);
-    #ifndef ONLINE_JUDGE
-    cerr << "Time: " << duration . count() / 1000 << endl;
-    #endif
-    return 0;
+ fast;
+ auto start1 = high_resolution_clock::now();
+ // #ifndef ONLINE_JUDGE
+ // freopen("shell.in", "r", stdin);
+ // freopen("shell.out", "w", stdout);
+ // #endif
+ int t=1;
+ cin>>t;
+ for(int i=1;i<=t;i++){
+     //google(i);
+     solve();
+ }
+ auto stop1 = high_resolution_clock::now();
+ auto duration = duration_cast<microseconds>(stop1 - start1);
+ #ifndef ONLINE_JUDGE
+ cerr << "Time: " << duration . count() / 1000 << endl;
+ #endif
+ return 0;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
